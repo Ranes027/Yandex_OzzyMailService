c440820b88ceedd17d014f82c0d07ec8c98bf2fc
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-03-04 17:29:31 +0300

    Add collectibles scripts

---
+++ OzzyMailService/Assets/Scripts/Components/Collectibles/CollectibleManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using OzzyMailService.Data;
+
+namespace OzzyMailService.Components.Collectibles
+{
+    public class CollectibleManager : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _valueText;
+
+        public int NumberOfCollectibles;
+
+        private void Start()
+        {
+            NumberOfCollectibles = Progress.Instance.PlayerInfo.Collectibles;
+            _valueText.text = NumberOfCollectibles.ToString();
+            transform.parent = null;
+        }
+
+        public void AddOne()
+        {
+            NumberOfCollectibles++;
+            _valueText.text = NumberOfCollectibles.ToString();
+        }
+
+        public void SaveToProgress()
+        {
+            Progress.Instance.PlayerInfo.Collectibles = NumberOfCollectibles;
+        }
+
+        public void SpendMoney(int value)
+        {
+            NumberOfCollectibles -= value;
+            _valueText.text = NumberOfCollectibles.ToString();
+        }
+    }
+}
+
