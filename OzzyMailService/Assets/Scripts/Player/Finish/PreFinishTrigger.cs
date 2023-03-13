e45e011da3ae5bbfb4681abf372a90787256e28a
Viacheslav Gerasimenko <supursoul@gmail.com>
2023-03-13 14:01:20 +0300

    Create PreFinishTrigger.cs

---
+++ OzzyMailService/Assets/Scripts/Player/Finish/PreFinishTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OzzyMailService.Player
+{
+    public class PreFinishTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            PlayerBehaviour playerBehaviour = other.attachedRigidbody.GetComponent<PlayerBehaviour>();
+            if (playerBehaviour != null)
+            {
+                playerBehaviour.StartPreFinishBehaviour();
+            }
+        }
+    }
+
+}
